28705a04a593f5bbe27574e3180a3dde01f4f09c
gagishmagi <gagi.shmagi@gmail.com>
2025-01-11 09:09:40 +0200

    fixed test

--- backend/tests/unit/test_articles_unit.py
+++ backend/tests/unit/test_articles_unit.py
@@ -13,6 +13,35 @@ class TestArticleSerializerUnit(TestCase):
         serializer = ArticleSerializer(self.article)
         data = serializer.data
 
-        self.assertEqual(data['title'], 'Wrong Title')
+        # self.assertEqual(data['title'], 'Wrong Title')
+        self.assertEqual(data['title'], self.article.title)
         self.assertEqual(data['description'], self.article.description)
+
+    def test_deserialize_article(self):
+        data = {
+            "title": "New Test Article",
+            "description": "This is a new test article"
+        }
+        serializer = ArticleSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+        article = serializer.save()
+        self.assertEqual(article.title, data['title'])
+        self.assertEqual(article.description, data['description'])
+
+    def test_update_article(self):
+        data = {
+            "title": "Updated Test Article",
+            "description": "This is an updated test article"
+        }
+        serializer = ArticleSerializer(self.article, data=data)
+        self.assertTrue(serializer.is_valid())
+
+        article = serializer.save()
+        self.assertEqual(article.title, data['title'])
+        self.assertEqual(article.description, data['description'])
+
+    def test_delete_article(self):
+        self.article.delete()
+        self.assertFalse(Article.objects.filter(id=self.article.id).exists())
 
